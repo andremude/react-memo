19345d8001bc34ec7f65f007667ad83302b3f236
Andres Munoz <andremude@hotmail.com>
2023-02-19 19:58:06 -0500

    add images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const Images = [
+    "https://icongr.am/devicon/angularjs-original.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/csharp-original.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/cplusplus-original.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/html5-original-wordmark.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/java-original-wordmark.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/javascript-original.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/php-original.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/python-original.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/ruby-original-wordmark.svg?size=128&color=currentColor",
+    "https://icongr.am/devicon/react-original-wordmark.svg?size=128&color=currentColor"
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {Images.map((image, index) => (
+        <img key={index} src={image} alt="icon" />
+      ))}
+    </main>
   );
 }
 
